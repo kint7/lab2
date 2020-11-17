3532b4369bb84a7c9b593282b8e72dd8ecf7ba0c
Ayzmil Amzah <2018276758@isiswa.uitm.edu.my>
2020-11-18 03:00:34 +0800

    second commit

---
+++ 2.1b.c
@@ -0,0 +1,23 @@
+#include <stdlib.h> /*needed to define exit()*/
+#include <unistd.h> /*needed for fork() and getpid()*/
+#include <stdio.h>  /*needed for printf()*/
+#include <sys/wait.h>
+int main(int argc, char**argv){
+	int pid; /*process ID*/
+	
+	switch(pid = fork()){
+	case 0:
+		printf("I am the child process: pid=%d\n", getpid());
+		break;
+
+	default:
+		wait(NULL);
+		printf("I am the parent process: pid=%d, child pid=%d\n", getpid(), pid);
+		break;
+
+	case -1:
+		perror("fork");
+		exit(1);
+	}
+	exit(0);
+}
